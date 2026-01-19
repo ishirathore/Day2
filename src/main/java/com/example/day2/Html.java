707ac25b6bf20d6c5d7e775424e5d432f132f6a6
Ishika Rathore <ishikarathore030@gmai.com>
2026-01-19 14:16:14 +0530

    this is day2

---
+++ src/main/java/com/example/day2/Html.java
@@ -0,0 +1,21 @@
+package com.example.day2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Html {
+    @GetMapping("/")
+
+    public String Hello(){
+        return "index";
+    }
+    @GetMapping("/contact")
+    public String Contact(){
+        return "contact";
+    }
+    @GetMapping("/about")
+    public String About(){
+        return "about";
+    }
+}
